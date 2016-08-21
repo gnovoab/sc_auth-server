7acf93465ec8e82501c97c7a4b7e8bd79817d5dd
gabriel.novoa <gnovoab@gmail.com>
2016-08-21 21:53:26 +0100

    initial import

---
+++ src/main/java/com/microservice/auth/Application.java
@@ -0,0 +1,30 @@
+
+//Namespace
+package com.microservice.auth;
+
+//Imports
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.ComponentScan;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * The Main Spring Boot Application class
+ */
+@ComponentScan
+@EnableAutoConfiguration
+@EnableEurekaClient
+public class Application {
+
+    /**
+     * The main method.
+     *
+     * @param args the arguments
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
